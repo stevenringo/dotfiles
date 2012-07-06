e14f495c267ff3747b386b6adb1ac2d4df6ce460
Zach Holman <your@example.com>
2012-07-06 22:01:56 +1000

    Putting my own flavour on things

---
+++ web/aliases.zsh
@@ -0,0 +1,7 @@
+alias tosass='pbpaste | sass-convert | pbcopy'
+alias toscss='pbpaste | sass-convert -Fcss -Tscss | pbcopy'
+alias sasstoscss='pbpaste | sass-convert -Fsass -Tscss | pbcopy'
+alias scsstosass='pbpaste | sass-convert -Fscss -Tsass | pbcopy'
+alias tohaml='pbpaste | html2haml -e | pbcopy'
+alias http='python -m SimpleHTTPServer'
+
